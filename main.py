fe4f0c045712801c651dcead79fe23ac52bd3c81
hudsonwg <hudson.w.goddard@gmail.com>
2023-12-19 16:39:11 -0500

    Added Custy Bumpy Sphere, Next up is make it responsive to sound/music

--- main.py
+++ main.py
@@ -68,20 +68,25 @@ def generate_torus_frame(A, B):
             returnFrame.append([xTerm, yTerm, zTerm])
     print(returnFrame)
     return returnFrame
-def generate_bumpy_sphere_frame(A, B):
+def generate_bumpy_sphere_frame(alpha, beta, gamma, A):
+    #make color change param
     RES1 = 0.1
     RES2 = 0.1
     returnFrame = []
-    PARAM1 = 4
-    PARAM2 = A*10
-    PARAM3 = A*2
-    SCALE_FACTOR = 9
+    PARAM1 = 5
+    PARAM2 = 2
+    PARAM3 = 2*alpha
+    SCALE_FACTOR = 10
     for theta in np.arange(0, math.pi * 2, RES1):
         for phi in np.arange(0, math.pi * 2, RES2):
             radius = SCALE_FACTOR*(1 + (1 / PARAM1) * math.sin(PARAM2 * theta) * math.sin(PARAM3 * phi))
             xTerm = radius*math.cos(theta)*math.sin(phi)
             yTerm = radius*math.sin(theta)*math.sin(phi)
-            zTerm = radius*math.sin(phi)
+            zTerm = radius*math.cos(phi)
+            #now adjust for rotation alpha, beta, gamma
+            xTerm = xTerm*math.cos(alpha)*math.cos(beta) + yTerm*math.cos(alpha)*math.sin(beta)*math.sin(gamma) - yTerm*math.sin(alpha)*math.cos(gamma) + zTerm*math.cos(alpha)*math.sin(beta)*math.cos(gamma)+zTerm*math.sin(alpha)*math.sin(gamma)
+            yTerm = xTerm*math.sin(alpha)*math.cos(beta) + yTerm*math.sin(alpha)*math.sin(beta)*math.sin(gamma) + yTerm*math.cos(alpha)*math.cos(gamma) + zTerm*math.sin(alpha)*math.sin(beta)*math.cos(gamma) - zTerm*math.cos(alpha)*math.sin(gamma)
+            zTerm = xTerm*(-1)*math.sin(beta) + yTerm*math.cos(beta)*math.sin(gamma) + zTerm*math.cos(beta)*math.cos(gamma)
             returnFrame.append([xTerm, yTerm, zTerm])
             #ρ(θ, φ) = 1 + 1/5sin(aθ) sin(bφ)
 
@@ -130,7 +135,40 @@ def generateAnimation(generate_frame):
         #DRAW ANIMATION HERE
         redrawGameWindow()
     pygame.quit()
+def generate_bumpy_sphere_animation():
+    FPS = 15
+    width = 1000
+    height = 1000
+    pygame.init()
+    win = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("TORUS FOREST EXPLORASAURUS")
+    x = 50
+    y = 50
+    vel = 5
 
+    def redrawGameWindow():
+        white = (255, 255, 255)
+        yellow = (255, 255, 0)
+        green = (0, 255, 255)
+        orange = (255, 100, 0)
+        Font = pygame.font.SysFont('timesnewroman', 30)
+        DIM = 15
+        for A in np.arange(0, 2*math.pi, 0.1):
+            win.fill((0, 0, 0))
+            data = generate_bumpy_sphere_frame(A, A, A, A)
+            for x in data:
+                # plt.scatter(x[0], x[1], color='black')
+                # DRAW TO COORDINATE
+                win.blit(Font.render(".", False, (255, 255, 255)), (20 * x[0] + width / 2, 20 * x[1] + height / 2))
+            pygame.time.Clock().tick(FPS)
+            pygame.display.update()
+
+    run = True
+    while run:
+        # pygame.time.delay(100)
+        # DRAW ANIMATION HERE
+        redrawGameWindow()
+    pygame.quit()
 if __name__ == '__main__':
     #Check first intuitions function for explanation on how to get started thinking about this
     #first_intuitions()
@@ -138,4 +176,5 @@ if __name__ == '__main__':
     #generateFramesDemo()
 
     #generate animation, pass in frame generator function
-    generateAnimation(generate_bumpy_sphere_frame)
+    #generateAnimation(generate_torus_frame)
+    generate_bumpy_sphere_animation()
